08a0d6f3b0a55848f0da17c7bd3d99bb70085c44
tobias-barakaa <ngairato@gmail.com>
2024-09-02 22:15:34 +0300

    stashing changes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <form>
+  <div className="container">
+    <h1>Sign Up</h1>
+    <p>To add recipe create an account.</p>
+    <hr />
+
+    <label ><b>Email</b></label>  
+    <input type="text" placeholder="Enter Email" name="email" required />
+ 
+
+    <label ><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required />
+
+    <label><b>Re-enter Password</b></label>
+    <input type="password" placeholder="Re-enter Password" name="psw-repeat" required />
+    
+    
+    <p>By creating an account you agree to our <a href="#" >Conditions and Requirements</a>.</p>
+
+    <div className="clearfix">
+      <button type="button" className="cancelbtn">Cancel</button>
+      <button type="submit" className="signupbtn">Sign Up</button>
+    </div>
+  </div>
+</form>
+
     </>
   )
 }
